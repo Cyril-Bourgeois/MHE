e2b1eeef1a859768df1fced54eb28aa4b7b9125a
Cyril-Bourgeois <156002197+Cyril-Bourgeois@users.noreply.github.com>
2024-01-10 10:07:18 +0100

    Update param_cout.py

--- MHE/param_cout.py
+++ MHE/param_cout.py
@@ -5,41 +5,41 @@ Created on Fri Nov 26 16:53:13 2021
 @author: bourgeoisc, neverren
 """
 ###############################################################################
-######################## Paramètre de couts ###################################
+######################## Cost pararameters ###################################
 ###############################################################################
+## PArameters for cost function (see function.py)
 
-
-#### Passages des mois en jours
+#### Month in days
 duree= 3600*24*365/12
-#### Passage des jours en mois
+#### day in months
 duree_jours_mois=365/12
-#### Prix electricité (€/kwh)
+#### electricity price (€/kwh)
 prix_elec= 0.08
-#### conversion kiloWattheure en unite fondamentale kg.m3/s^3 (1W= 1 kg.m3/s^3 et 1Wh= 1000*3600W)
+####  kiloWattheure  conversion in fundamental unit :kg.m3/s^3 (1W= 1 kg.m3/s^3 and 1Wh= 1000*3600W)
 conversion_kwh_kgm3s2= 1000*3600
-#### Masse volumique de l'eau (kg/m3)
+#### Water density (kg/m3)
 rho=1000
-#### Constante de gravité (m/s2)
+#### Gravity constant (m/s2)
 g= 9.81
-#### rendement de la pompoe (sans unité)
+#### pump efficiency (unitless)
 rdt_pompe=0.8
-#### Perméabilité de la nappe (m/s)
+#### permeability of the water table  (m/s)
 K_n= 7e-5
-#### Epaisseur de la nappe (m)
+#### thickness of water table (m)
 H_n=60
-####  Constante pour la nature de la canalisation
+####   Constant for pipe type
 k = 0.003185
 
-### prix de l'eau pour la défaillance
+### water price for failure function
 p_eau=3
 p_max_eau=100
 
-#### Perte entre le lacher Saint-Michel et les Ouvrages
-perte_lacher=0.6
-perte_lacher_Kerrous=1
+#### Loss between Dams and abstraction works
+perte_lacher=0.6 #Loss for Brenillis (long canal between reservoir and withdrawal point)
+perte_lacher_Kerrous=1 # Loss fos Kerrous (pump directly in Dams)
 
-#### Couttransaction USines SMA
+#### SMA Cost transaction
 param_cout_SMA=0.2
 
-#### cout pr prio Aulne Vs SM
+#### opportunity cost(cost to prio river on Dams)
 cout_lacher=0.1
