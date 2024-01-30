1300bfd72b8acdaa516cef6c6727ec6cf3198e6e
Cyril-Bourgeois <156002197+Cyril-Bourgeois@users.noreply.github.com>
2024-01-30 18:46:38 +0100

    Update fonctions.py

--- MHE/fonctions.py
+++ MHE/fonctions.py
@@ -4,37 +4,38 @@ Created on Fri Nov 26 17:10:42 2021
 
 @author: bourgeoisc,neverren
 """
-
-def cout_prelevement(flux,profondeur,type_eau):
-    debit= flux/duree # Conversio flux mensuel de m3/mois à m3/sec
-    S= debit/((2/3)*K_n * 2* H_n )# Formule de Porchet
-    w_pompe = rho*g*debit*(profondeur+type_eau* S)
-    cout_prel=(w_pompe/rdt_pompe)*duree*prix_elec/conversion_kwh_kgm3s2
+## Function for withdrawal cost
+def cout_prelevement(flow, depth, water_type):
+  flow_s= flow/duration # Convert monthly flow from m3/month to m3/sec
+    S=  flow_s/((2/3)*K_n * 2* H_n )# Porchet Formula
+    w_pompe = rho*g*flow_s*(depth+water_type* S)
+    cout_prel=(w_pompe/rdt_pompe)*duration*price_elec/conversion_kwh_kgm3s2
     return cout_prel 
 
-
+## Function for transport cost
 def cout_transport(flux,D,L):
-    debit=flux/duree
-    deltaH=k*(1/(1+3*sqrt(D/2)))*(debit*debit)/(D*D)
-    w_trans=rho*g*debit*deltaH
-    output=w_trans*L*duree*prix_elec/conversion_kwh_kgm3s2
+    flow_s= flow/duration
+    deltaH=k*(1/(1+3*sqrt(D/2)))*(flow_s*flow_s)/(D*D)
+    w_trans=rho*g*flow_s*deltaH
+    output=w_trans*L*duration*price_elec/conversion_kwh_kgm3s2
     return output
 
-
+## Function for linear  failure cost (not used)
 def cout_defaillance(flux):   
     output= 9999*flux
     return output
 
-### mettre 100 (p_eau_max en paream!)
+### Function for quadratic failure cost
 def cout_defaillance2(flux,demande):   
     output= flux* p_eau + flux*flux*(p_max_eau-p_eau)/(2*demande+1)
     return output
 
+###Function for treatment cost
 def cout_traitement(flux,cout):
     output= flux*cout
     return output
 
-
+###Function for transaction cost
 def cout_transaction(flux,param):
     output = flux*param
     return output
